73c174fd793353baec20b7b4df38dccc018524ec
Rafael Correia da Silva <rcs.biotec@gmail.com>
2018-06-05 12:57:19 -0300

    Create qsub_instructions.sh

---
+++ qsub_instructions.sh
@@ -0,0 +1,13 @@
+
+# How to: QSUB
+
+echo "command" | qsub \
+-cwd \
+-N "name" \
+-m base -M rcs.biotec@gmail.com \
+mf=30G
+
+# example usage
+
+echo "bash /home/externo/danielgp/bin/rsem-eval-rcsilva.sh" | qsub \
+-N alignOK -cwd -l mf=30G
