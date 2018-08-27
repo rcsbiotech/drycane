8ddd40fdd982654092ce3208239eb1179bcf0476
Rafael Correia da Silva <rcs.biotec@gmail.com>
2018-08-27 15:54:16 -0300

    Update 02_VisualClusters.R
    Added Tolerant and Susceptible

--- Rscripts/02_VisualClusters.R
+++ Rscripts/02_VisualClusters.R
@@ -1,150 +1,172 @@
-
-## Plot per cluster ##
-
-# Sections:
-# 1. Shiny inputs
-# 2. Plot generation
-
 # [ .. 1. Shiny User Inputs Processing ..] #
+
+# Limpar arquivos anteriores
+rm(kmeans)
+rm(mainDF)
+rm(DFSubset)
+rm(clusterA.values)
+rm(clusterB.values)
+rm(SigDay)
+rm(c(Tag30Tolerant, Tag60Tolerant, Tag90Tolerant,
+     Tag30Susceptible, Tag60Susceptible, Tag90Susceptible))
 
 # Input01: Qual data DEG (30, 60, 90)
 # Diferencialmente expresso na data..
 InputDate <- 30
 
 # Quais clusters?
-ClusterA <- 01
-ClusterB <- 08
+clusterA <- 07
+clusterB <- 09
 
-# Limites para o eixo Y
-Yupper <- 3
-Ylower <- -3
+# Qual tag consultar?
+QueryTag = "TRINITY_DN32024_c0_g1_i3"
 
-# Init modelos lineares
-init = T
+## Filtrando os valores para cada tag
+# Em 30 dias
+Tag30Tolerant <- subset(Sig30, (Tag == QueryTag & Genotype == "T"))
+Tag30Susceptible <- subset(Sig30, (Tag == QueryTag & Genotype == "S"))
+
+# Em 60 dias
+Tag60Tolerant <- subset(Sig60, (Tag == QueryTag & Genotype == "T"))
+Tag60Susceptible <- subset(Sig60, (Tag == QueryTag & Genotype == "S"))
+
+# Em 90 dias
+Tag90Tolerant <- subset(Sig90, (Tag == QueryTag & Genotype == "T"))
+Tag90Susceptible <- subset(Sig90, (Tag == QueryTag & Genotype == "S"))
 
 # Seleção do kMeans para o cluster
 if (InputDate == 30) {
   kmeans <- kMeans30
+  SigDay <- Sig30
+  PickedTagTolerant <- Tag30Tolerant
+  PickedTagSusceptible <- Tag30Susceptible
 } else if (InputDate == 60) {
   kmeans <- kMeans60
+  SigDay <- Sig60
+  PickedTagTolerant <- Tag60Tolerant
+  PickedTagSusceptible <- Tag60Susceptible
 } else if (InputDate == 90) {
   kmeans <- kMeans90
+  SigDay <- Sig90
+  PickedTagTolerant <- Tag90Tolerant
+  PickedTagSusceptible <- Tag90Susceptible
 }
 
+# Montagem da matriz de entrada
 kmeans$centers <- cbind(kmeans$centers, 30, 60, 90)
 colnames(kmeans$centers)[4:6] <- c("I30", "I60", "I90")
+mainDF <- as.data.frame(kmeans$centers)
+DFSubset <- subset(mainDF, rownames(mainDF) == clusterA | rownames(mainDF) == clusterB)
 
-## <.. End of Shiny Input/Processing ..> ##
+# Escolha de cores (T, S)
+colorPickerA = "limegreen"
+colorPickerB = "orangered4"
 
-## [.. 2. Figure generation .. ] ##
-ggplot(AmbosClustersSig[[InputDate/30]][,InputDate/30],
-       aes(x=c(30),
-           y=AmbosClustersSig[[InputDate/30]][,InputDate/30][names(kmeans$cluster == ClusterA |
-                                                                        kmeans$cluster == ClusterB)])) +
-  
-  # Coordenadas cartesianas: ampliação da figura
-  coord_cartesian(ylim=c(Ylower, Yupper)) +
-  
-  # Adição de rótulos
-  ylab("Log2FC") +
-  xlab("Course of time") +
-  
-  # Remover o eixo ys
-  theme_minimal() +
-  theme(axis.text.y = element_blank()) +
-  
-  # Escala do eixo y
-  #scale_y_discrete(breaks=c(2), limits=c(-4, 4)) +
-  
-                ## [.. Adição dos agrupamentos ..] ##
-  
-  # Adição do primeiro cluster.
-  # mapping = de onde sairão os dados;
-  # x = c(dia), o valor para o eixo x;
-  # y = os dados a serem retirados do clusterA
+# Separação dos clusters
+clusterA.values <- mainDF[clusterA,1:6]
+clusterB.values <- mainDF[clusterB,1:6]
 
-  geom_point(mapping=
-                aes(x=c(30),
-                    y=(AmbosClustersSig[[InputDate/30]][,1][names(kmeans$cluster[kmeans$cluster == ClusterA])])),
-              size=0.5, #width = 3,
-             color="steelblue") +
+# Forja do plot básico
+ggplot(mainDF) +
+  # Pontos dos clusters a cada período, por cores
+  ## Cluster A
+  geom_point(aes(x=I30[clusterA], y=R30[clusterA]), color="blue") +
+  geom_point(aes(x=I60[clusterA], y=R60[clusterA]), color="blue") +
+  geom_point(aes(x=I90[clusterA], y=R90[clusterA]), color="blue") +
   
-  geom_point(mapping=
-                aes(x=c(60),
-                    y=(AmbosClustersSig[[InputDate/30]][,2][names(kmeans$cluster[kmeans$cluster == ClusterA])])),
-              size=0.5, #width = 3,
-             color="steelblue") +
+  ## Cluster B
+  geom_point(aes(x=I30[clusterB], y=R30[clusterB]), color="tomato") +
+  geom_point(aes(x=I60[clusterB], y=R60[clusterB]), color="tomato") +
+  geom_point(aes(x=I90[clusterB], y=R90[clusterB]), color="tomato") +
   
-  geom_point(mapping=
-                aes(x=c(90),
-                    y=(AmbosClustersSig[[InputDate/30]][,3][names(kmeans$cluster[kmeans$cluster == ClusterA])])),
-              size=0.5, #width = 3,
-             color="steelblue") +
+  # Rótulos dos gráficos e estéticas adicionais
+  xlab("Tempo (dias)") +
+  ylab("Comportamento em relação à não-estressada") +
+  geom_hline(mapping=aes(yintercept=0), linetype="dotted") +
   
-  # Adição do segundo cluster (mesma lógica)
-  
-  geom_point(mapping= 
-                aes(x=c(30),
-                    y=(AmbosClustersSig[[InputDate/30]][,1][names(kmeans$cluster[kmeans$cluster == ClusterB])])),
-              size=0.5,
-             #width = 3,
-             color="tomato3") +
-  
-  geom_point(mapping=
-                aes(x=c(60),
-                    y=(AmbosClustersSig[[InputDate/30]][,2][names(kmeans$cluster[kmeans$cluster == ClusterB])])),
-              size=0.5,
-             #width = 3,
-             color="tomato3") +
-  
-  geom_point(mapping=
-                aes(x=c(90),
-                    y=(AmbosClustersSig[[InputDate/30]][,3][names(kmeans$cluster[kmeans$cluster == ClusterB])])),
-              size=0.5, #width = 3,
-             color="tomato3") +
-  
-  geom_hline(yintercept=0, linetype="dashed", size=0.5) +
-  
-                  ## [.. Adição de centróides ..] ##
-  
-  # Adição do centróide do cluster A
-  
-  geom_point(mapping=
-               aes(x=c(30, 60, 90),
-                   y=c(kmeans$centers[ClusterA, 1],
-                       kmeans$centers[ClusterA, 2],
-                       kmeans$centers[ClusterA, 3])), size=10, color="steelblue") +
-  
-  # Adição do centróide do cluster B
-  geom_point(mapping=
-               aes(x=c(30, 60, 90),
-                   y=c(kmeans$centers[ClusterB, 1],
-                       kmeans$centers[ClusterB, 2],
-                       kmeans$centers[ClusterB, 3])), size=10, color="tomato3") +
-  
-  # Adição da linha
-    geom_line(mapping=
-               aes(x=c(30, 60, 90),
-                   y=c(kmeans$centers[ClusterA, 1],
-                       kmeans$centers[ClusterA, 2],
-                       kmeans$centers[ClusterA, 3])), size=1, color="steelblue") +
-    
+  # Linha de tendência dos clusters selecionados
+  ### Cluster A
+  ## 30 pra 60
+  geom_line(data=DFSubset,
+    mapping=aes(x=c(30, 60),
+                y=c(R30[1], R60[1])),
+    color="blue",
+    linetype="twodash") +
 
-    geom_line(mapping=
-             aes(x=c(30, 60, 90),
-                 y=c(kmeans$centers[ClusterB, 1],
-                     kmeans$centers[ClusterB, 2],
-                     kmeans$centers[ClusterB, 3])), size=1, color="tomato3")
+  ## 60 pra 90, cluster A
+  geom_line(data=DFSubset,
+          mapping=aes(x=c(60, 90),
+                      y=c(R60[1], R90[1])),
+          color="blue",
+          linetype="twodash") +
 
-  # Plotar junto do gráfico, as tags por cluster
-
-  geom_point(mapping=
-               aes(x=c(30),
-                   y=(AmbosClustersSig[[InputDate/30]][,1][as.data.frame(
-                     AmbosClustersSig$at30,stringsAsFactors=F)$Tag == as.data.frame(
-                       diff.at30$Tag,stringsAsFactors=F)),
-             size=0.5, #width = 3,
-             color="steelblue"))
-
-
+  ### Cluster B
+  ## 30 pra 60
+  geom_line(data=DFSubset,
+          mapping=aes(x=c(30, 60),
+                      y=c(R30[2], R60[2])),
+          color="tomato",
+          linetype="twodash") +
   
+  ## 60 pra 90, cluster A
+  geom_line(data=DFSubset,
+            mapping=aes(x=c(60, 90),
+                        y=c(R60[2], R90[2])),
+            color="tomato",
+            linetype="twodash") +
+
+  ## Verificando valores da tag para suscetível e tolerante
+  ## Adicionando os pontos para tag escolhida, tolerante e suscetível
+  
+  
+  ## TOLERANTE ##
+  # Tag escolhida em 30 (Tolerant)
+  geom_point(data=PickedTagTolerant, mapping=aes(x=30,y=as.double(PickedTagTolerant$R30)),
+             color=colorPickerA) + 
+
+  # Tag escolhida em 60
+  geom_point(data=PickedTagTolerant, mapping=aes(x=60,y=as.double(PickedTagTolerant$R60)),
+           color=colorPickerA) + 
+
+  # Tag escolhida em 60
+  geom_point(data=PickedTagTolerant, mapping=aes(x=90,y=as.double(PickedTagTolerant$R90)),
+           color=colorPickerA) + 
+
+  # Linha de 30 para 60, tolerante
+  geom_line(data=PickedTagTolerant[1:2,],
+            mapping=aes(x=c(30, 60),
+                        y=c(as.double(PickedTagTolerant$R30), as.double(PickedTagTolerant$R60))),
+            color=colorPickerA, size=2) +
+
+  # Linha de 60 para 90, tolerante
+  geom_line(data=PickedTagTolerant[2:3,],
+          mapping=aes(x=c(60, 90),
+                      y=c(as.double(PickedTagTolerant$R60), as.double(PickedTagTolerant$R90))),
+          color=colorPickerA, size=2) +
+  
+  ## SUSCETÍVEL ##
+  # Tag escolhida em 30 (Tolerant)
+  geom_point(data=PickedTagSusceptible, mapping=aes(x=30,y=as.double(PickedTagSusceptible$R30)),
+           color=colorPickerB) + 
+  
+  # Tag escolhida em 60
+  geom_point(data=PickedTagSusceptible, mapping=aes(x=60,y=as.double(PickedTagSusceptible$R60)),
+             color=colorPickerB) + 
+  
+  # Tag escolhida em 60
+  geom_point(data=PickedTagSusceptible, mapping=aes(x=90,y=as.double(PickedTagSusceptible$R90)),
+             color=colorPickerB) + 
+  
+  # Linha de 30 para 60, tolerante
+  geom_line(data=PickedTagSusceptible[1:2,],
+            mapping=aes(x=c(30, 60),
+                        y=c(as.double(PickedTagSusceptible$R30), as.double(PickedTagSusceptible$R60))),
+            color=colorPickerB, size=2) +
+  
+  # Linha de 60 para 90, tolerante
+  geom_line(data=PickedTagSusceptible[2:3,],
+            mapping=aes(x=c(60, 90),
+                        y=c(as.double(PickedTagSusceptible$R60), as.double(PickedTagSusceptible$R90))),
+            color=colorPickerB, size=2) +
+  
+  scale_color_identity(name = 'the fill', guide = 'legend', labels = c('m1'))
